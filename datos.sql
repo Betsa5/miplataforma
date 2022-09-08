82f916fdd59ea7932d963b5faf615b7f76527ee8
* <betsaida.carvajal@utelvt.edu.ec>
2022-09-08 14:21:35 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,4 @@
+create database miplataforma;
+use miplataforma;
+
+create table personsa(idepersona int ,nombres varchar(20),apellidos varchar(20), cedula varchar(10),idgenero int , fechanacimiento date ,idnacionalidad int ,identia int,idestadocivil int);
